a1e9d682b6107ac0469d302dbaaa98776fe9f99e
Lau <lponscastello@gmail.com>
2021-03-24 10:56:33 +0100

    checkpoint 2

--- examen.py
+++ examen.py
@@ -3,16 +3,47 @@ def get_list(fichero_palabras):
 
     dic = {}
     lineas_fichero = f.readline()
+
+    if lineas_fichero == "":
+        raise ValueError("El fichero está vacío")
+    else:
+        contador_letras = 0
+        while lineas_fichero != "" :
+            palabras_linea = lineas_fichero.split(" ")
+            for x in palabras_linea:
+                if x.isalpha():
+                    for i in x:
+                        contador_letras = contador_letras + 1
+
+                    lista_palabras = dic[contador_letras]
+
+                    for j in lista_palabras:
+                        if x != lista_palabras:
+                            lista_palabras.append(x)
+
+                            dic.update({contador_letras: lista_palabras})
+                else:
+                    raise ValueError("Se ha encontrado un elemento que no es una palabra, el fichero solo debe contener palabras.")
+            
+    return dic
+
+
+def crear_lista_libros():
+
     
-    contador_letras = 0
-
-    while lineas_fichero != "" :
-        palabras_linea = lineas_fichero.split()
-        for x in palabras_linea:
-            for i in x:
-                contador_letras = contador_letras + 1
-                
+    
+    lista_libros = []
+    
+    return lista_libros
 
 
-def mas_antiguos():
+def mas_antiguos(lista_libros):
+
+
+
+
+
+lista_libros = crear_lista_libros()
+mas_antiguos(lista_libros)
+
 
--- libro.py
+++ libro.py
@@ -4,4 +4,13 @@ class Libro:
         self.__autor = autor
         self.__titulo = titulo
         self.__anyo = anyo
+
+        def get_autor(self):
+            return self.__autor
+
+        def get_titulo(self):
+            return self.__titulo
+
+        def get_anyo(self):
+
         
